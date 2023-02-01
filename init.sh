5408fdb1d8e1ea814773fef7032ea175d7c9b570
Chris Kelly <chris.kelly@koop.overheid.nl>
2023-02-01 06:23:59 +0100

    mlsh first version

--- init.sh
+++ init.sh
@@ -2,23 +2,25 @@
 
 echo "Initializing ml-shell..."
 getScriptDir() {
-  local scriptDir=$(dirname $0)
+  local scriptPath=$(readlink -f $1)
+  local scriptDir=$(dirname $1)
   if [ "$scriptDir" = "." ]; then
     scriptDir=$(pwd)
   fi
   echo $scriptDir
 }
 
-export MLSH_TOP_DIR=$(getScriptDir)
-alias qc="cd $MLSH_TOP_DIR; bash scripts/qc.sh"
-alias mlsh="cd $MLSH_TOP_DIR; bash scripts/mlsh.sh"
+export MLSH_TOP_DIR=$(getScriptDir $0)
+alias qc="bash $MLSH_TOP_DIR/scripts/qc.sh"
+alias mlsh="bash $MLSH_TOP_DIR/scripts/mlsh.sh"
+alias mlsh:go="cd $MLSH_TOP_DIR"
 
-
-if [ ! -f "$MLSH_TOP_DIR/env.sh" ]; then
+if [ ! -f "$HOME/.mlshrc" ]; then
+  echo "No ~/.mlshrc file found."
   echo "Creating a default .mlshrc file in your home directory."
   echo "Please modify as needed."
-  cp $MLSH_TOP_DIR/.mlshrc.example ~/.mlshrc
-  exit
+  cp $MLSH_TOP_DIR/mlshrc.template ~/.mlshrc
 fi
 
+echo "Sourcing ~/.mlshrc"
 source ~/.mlshrc
--- scripts/mlsh.sh
+++ scripts/mlsh.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source $MLSH_TOP_DIR/scripts/common.sh
 mlsh() {
   local cmd=$1
   shift
@@ -9,17 +10,31 @@ mlsh() {
       cd $MLSH_TOP_DIR
       bash scripts/qc.sh "${args[@]}"
       ;;
+
     "eval")
       doEval "${args[@]}"
       ;;
+
     "fetch")
       fetch "${args[@]}"
       ;;
+
     "corb")
+      if [ ! -f "$CORB_JAR" ]; then
+        echo "Please set CORB_JAR in your ~/.mlshrc file."
+        return
+      fi
+      if [ ! -f "$XCC_JAR" ]; then
+        echo "Please set XCC_JAR in your ~/.mlshrc file."
+        return
+      fi
       runCorb "${args[@]}"
       ;;
     "init")
       source $MLSH_TOP_DIR/init.sh
+      ;;
+    "help")
+      showHelp ${args[@]}
       ;;
     *)
       echo "Unknown command [$cmd]"
@@ -29,11 +44,68 @@ mlsh() {
 }
 
 showHelp() {
-  echo ""
-  echo "mlsh init                                       # source this file"
-  echo "mlsh qc [pull|push]                             # pull/push from/to query console"
-  echo "mlsh corb <task> <job> <threads> <batchSize>    # run corb task"
-  echo "mlsh eval <script> <db> <vars>                  # run eval script"
-  echo "mlsh fetch <endpoint> <rest>                    # fetch from ml"
-  echo ""
+  local cmd=$1
+  case $cmd in
+    "qc")
+      echo "Usage: mlsh qc [pull|push]"
+      echo ""
+      echo "Pulls or pushes the query console from/to the database."
+      echo ""
+      echo "Examples:"
+      echo " mlsh qc pull"
+      echo " mlsh qc push"
+      ;;
+    "eval")
+      echo "Usage: mlsh eval <script> <db> <vars>"
+      echo ""
+      echo "Runs an eval script against the database."
+      echo ""
+      echo "Examples:"
+      echo " mlsh eval /path/to/script.xqy App-Services"
+      echo " mlsh eval /path/to/script.xqy App-Services \"var1=value1&var2=value2\""
+      ;;
+    "fetch")
+      echo "Usage: mlsh fetch <endpoint> <rest>"
+      echo ""
+      echo "Fetches a resource from the database."
+      echo ""
+      echo "Examples:"
+      echo " mlsh fetch /manage/v2/databases"
+      echo " mlsh fetch /manage/v2/databases App-Services"
+      ;;
+    "corb")
+      echo "Usage: mlsh corb <task> <job> <threads> <batchSize>"
+      echo ""
+      echo "Runs a corb task."
+      echo ""
+      echo "Examples:"
+      echo " mlsh corb /path/to/task.xqy /path/to/job.xml 4 100"
+      ;;
+    "help")
+      echo "Usage: mlsh help <command>"
+      echo ""
+      echo "Shows help for a command."
+      echo ""
+      echo "Examples:"
+      echo " mlsh help qc"
+      echo " mlsh help eval"
+      echo " mlsh help fetch"
+      echo " mlsh help corb"
+      ;;
+    *)
+      echo ""
+      echo "mlsh <command> <args>                           # run a command"
+      echo ""
+      echo "Commands:"
+      echo " mlsh init                                       # source this file"
+      echo " mlsh qc [pull|push]                             # pull/push from/to query console"
+      echo " mlsh corb <task> <job> <threads> <batchSize>    # run corb task"
+      echo " mlsh eval <script> <db> <vars>                  # run eval script"
+      echo " mlsh fetch <endpoint> <rest>                    # fetch from ml"
+      echo " mlsh help <command> .                           # for more info on a command"
+      echo ""
+      ;;
+  esac
 }
+
+mlsh $@
