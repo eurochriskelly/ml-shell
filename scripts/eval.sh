776246ce961f6ea6bebf66bc4d8d5387db152ab1
Chris Kelly <chris.kelly@koop.overheid.nl>
2023-02-08 20:05:31 +0100

    eval command can execute any xquery file

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -2,18 +2,39 @@
 
 source $MULSH_TOP_DIR/scripts/common.sh
 
+showHelp() {
+  echo "Usage: mulsh eval [options] <script> <database> <params>"
+  echo "Options:"
+  echo "  -s|--script <script>   The script to run"
+  echo "  -d|--database <database>   The database to run the script against"
+  echo "  -p|--params <params>   The parameters to pass to the script"
+  echo "  -h|--help              Show this help"
+}
+
 echo $(pwd)
 run() {
+  local database=$ML_CONTENT_DB
   while [[ $# -gt 0 ]];do
     case $1 in
-      --script|-s) shift;script=$1;shift
+      --script|-s)
+        shift;script=$1;shift
+        # Check if file has extension. If yes then remove it.
+        # It will be found and added by the script
+        if [[ $script == *.* ]]; then
+          script=${script%.*}
+        fi
         ;;
-      --params|-p) shift;params=$1;shift
+
+      --params|-p)
+        shift;params=$1;shift
         ;;
-      --database|-d) shift;database=$1;shift
+
+      --database|-d)
+        shift;database=$1;shift
         ;;
       *)
         echo "Unknown argument [$1]"
+        showHelp
         shift
         ;;
     esac
